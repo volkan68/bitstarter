5900b8c52dfcf01cf072393caa1377873a1cb2a3
Volkan Rivera <me@volkanrivera.com>
2013-07-05 08:47:47 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var Text = require('buffer');
+var buff = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    Text = new Buffer(1024);
-    Text = fs.readFile("index.html");
-    var Message = Text.toString();
-    response.send(Message);
-//  response.send('Hello World2!');
+    buff = fs.readFileSync("./index.html");
+    var Text = buff.toString('utf8');
+    response.send(Text);
 });
 
 var port = process.env.PORT || 5000;
